c89cffbfe6e887accb322022950d40ce570eb453
bbesito66 <nicolastg44@gmail.com>
2025-09-01 01:28:04 -0500

    Agrego pruebas

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_random_dog(client):
+    response = client.get("/random-dog")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert "message" in data
+    assert "status" in data
+    assert data["status"] == "success"
